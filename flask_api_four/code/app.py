9a0ca8454f4038e6915afe6b024955e00e813d83
Rich Boothe <richardjboothe@gmail.com>
2017-12-06 22:29:52 -0600

    duplicate code

---
+++ flask_api_four/code/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from user import UserRegister
+from item import Item, ItemList
+
+app = Flask(__name__)
+app.secret_key = 'changeAndHide'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':  # if the name of this file is the file we run, then invoke app.run
+	app.run(port=4400, debug=True)
